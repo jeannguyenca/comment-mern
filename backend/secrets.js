31d6e4f66af793cdafc0734c44e72ac25f58eca2
Jean <jeannguyen.ca@gmail.com>
2018-10-13 18:03:55 -0700

    Fix secret

--- backend/secrets.js
+++ backend/secrets.js
@@ -1,5 +1,6 @@
 const secrets = {
-    dbUri: process.env.DB_URI
+    // dbUri: process.env.DB_URI
+    dbUri: "mongodb://jeannguyen:huonggiang252326@ds119273.mlab.com:19273/comment-mern"
 };
 // mongo ds119273.mlab.com:19273/comment-mern -u jeannguyen -p huonggiang252326
 //

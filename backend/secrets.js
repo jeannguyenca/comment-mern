599a43ca304d4d2fedd64d5087fa9532a9daa9db
Jean <jeannguyen.ca@gmail.com>
2018-10-15 20:16:44 -0700

    Add heroku mLab

--- backend/secrets.js
+++ backend/secrets.js
@@ -1,7 +1,10 @@
 const secrets = {
     // dbUri: process.env.DB_URI
-    dbUri: "mongodb://jeannguyen:huonggiang252326@ds119273.mlab.com:19273/comment-mern"
+    dbUri: "mongodb://heroku_fvk6s9j4:heroku_fvk6s9j4@ds037498.mlab.com:37498/heroku_fvk6s9j4"
 };
+
+//heroku
+//mongodb://heroku_fvk6s9j4:heroku_fvk6s9j4@ds037498.mlab.com:37498/heroku_fvk6s9j4
 // mongo ds119273.mlab.com:19273/comment-mern -u jeannguyen -p huonggiang252326
 //
 // export DB_URI=mongodb://jeannguyen:huonggiang252326@ds119273.mlab.com:19273/comment-mern
